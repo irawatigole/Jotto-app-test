2a08bb0ffcce6fe244c8e9459a862921c05e7e93
irawati <irawati.gole@costrategix.com>
2020-05-07 14:20:03 +0530

    third commit

--- src/Test/TestUtils.js
+++ src/Test/TestUtils.js
@@ -1,4 +1,12 @@
 import checkPropTypes from 'check-prop-types';
+import rootReducer from '../reducers';
+import { middlewares } from '../configureStore';
+import { createStore, applyMiddleware } from 'redux';
+
+export const storeFactory = (initialState) => {
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+    return createStoreWithMiddleware(rootReducer, initialState);
+  }
 
 export const findByTestAttr = (wrapper, val) => {
     return wrapper.find(`[data-test="${val}"]`);
